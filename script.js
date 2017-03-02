abbc9ccf0c33b8d03fc6dbc3aaf6d035bb4c3fa3
Darleny Mendoza <darleny2013madrid@gmail.com>
2017-03-02 03:37:56 +0000

    House was made

---
+++ script.js
@@ -0,0 +1,15 @@
+TinyTurtle.apply(window);
+right(90);
+forward(64);
+left(140);
+forward(43);
+left(82);
+forward(43);
+right(-50);
+forward(45);
+left(88);
+forward(64);
+left(90);
+forward(45);
+
+ 
